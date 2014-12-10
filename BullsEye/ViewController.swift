e134efbe8572131b762cb2099e40c86e94f49e4f
Fadie Hannona <fadiehannona@gmail.com>
2014-12-10 18:17:32 +0000

    score now appears on label

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var currentValue: Int = 0
-    var targetValue: Int = 0
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,11 +37,14 @@ class ViewController: UIViewController {
     
     func updateLabels() {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
     }
 
     @IBAction func showAlert() {
         let difference = abs(targetValue - currentValue)
         let points = 100 - difference
+        
+        score += points
         
         let message = "You scored \(points) points!"
         
